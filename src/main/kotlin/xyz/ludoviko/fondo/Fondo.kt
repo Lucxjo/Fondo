8d20f9090cbb6ea4dae11a35a3e4a1b5466d3525
Ludoviko <me@ludoviko.xyz>
2021-07-02 20:48:26 +0100

    Basic project setup.

--- src/main/kotlin/xyz/ludoviko/fondo/Fondo.kt
+++ src/main/kotlin/xyz/ludoviko/fondo/Fondo.kt
@@ -1,8 +1,8 @@
-package xyz.ludoviko.clean
+package xyz.ludoviko.fondo
 
 import net.fabricmc.api.ModInitializer
 
-object Clean : ModInitializer{
+object Fondo : ModInitializer {
     override fun onInitialize() {
         println("working")
     }
